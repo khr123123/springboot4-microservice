93c44463ff4efc31bf3706eaff7df8234b700602
3031208 <21@qwe>
2025-11-25 11:11:24 +0900

    1. init model

---
+++ user-service/user-api/src/test/java/org/khr/minibatis/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khr.minibatis.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ user-service/user-impl/src/main/java/org/khr/minibatis/userimpl/UserImplApplication.java
@@ -0,0 +1,13 @@
+package org.khr.minibatis.userimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserImplApplication.class, args);
+    }
+
+}
